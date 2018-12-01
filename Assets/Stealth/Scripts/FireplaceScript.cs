45393fa73a0ac99d554e729d0ae106ade73f97e4
Guillaume Ambrois <guillaume.ambrois@gmail.com>
2018-12-01 18:50:26 +0100

    Cleanup stealth scene and files

---
+++ Assets/Stealth/Scripts/FireplaceScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireplaceScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        col.gameObject.GetComponent<SantaController>().SetFireplace(this);
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        col.gameObject.GetComponent<SantaController>().UnsetFireplace(this);
+    }
+}
