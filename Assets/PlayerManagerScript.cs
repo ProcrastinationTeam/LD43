9beccd58445b19cf79fa810af827707187457d20
LeRyokan <tixier.lucas@gmail.com>
2018-12-01 17:15:06 +0100

    First UI finish

---
+++ Assets/PlayerManagerScript.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManagerScript : MonoBehaviour {
+
+    int maxWantedValue = 10;
+    public int wantedIndicator = 0;
+
+    int maxGift = 100;
+    public int currentGiftCount = 5;
+
+    public float fuelPerSecond = 2.0f;
+
+    float maxFuel = 100.0f;
+    public float currentFuel = 100.0f;
+
+    float timerForFuel = 0.0f;
+
+    public UIManagerScript uiManagerScript;
+
+    public int score = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ConsumeFuel(2 * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.KeypadPlus))
+        {
+            IncreaseWantedIndicator(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.KeypadMinus))
+        {
+            DecreaseWantedIndicator(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+
+        //timerForFuel += Time.deltaTime;
+        //if(timerForFuel > 1.0f)
+        //{
+            //timerForFuel = 0.0f;
+            
+       // }
+
+
+    }
+
+    public void IncreaseWantedIndicator(int value)
+    {
+        if( wantedIndicator + value > maxWantedValue)
+        {
+            wantedIndicator = maxWantedValue;
+        }
+        else
+        {
+            wantedIndicator += value;
+        }
+
+    }
+
+    public void DecreaseWantedIndicator(int value)
+    {
+        if (wantedIndicator - value < 0)
+        {
+            wantedIndicator = 0;
+        }
+        else
+        {
+            wantedIndicator -= value;
+        }
+  
+    }
+
+    public void IncreaseScore(int value)
+    {
+        score += value;
+    }
+
+    public void Shoot()
+    {
+        if(currentGiftCount - 1 < 0 )
+        {
+            currentGiftCount = 0;            
+        }
+        else
+        {
+            currentGiftCount -= 1;
+        }
+
+    }
+
+    public void ConsumeFuel(float value)
+    {
+        if (currentFuel - value < 0.0f)
+        {
+            currentFuel = 0.0f;
+        }
+        else
+        {
+            currentFuel -= value;
+        }
+        
+    }
+
+    public void AddGift(int value)
+    {
+        if(currentGiftCount + value > 100)
+        {
+            currentGiftCount = 100;
+        }
+        else
+        {
+            currentGiftCount += value;
+        }
+    }
+
+}
---
+++ Assets/UIManagerScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManagerScript : MonoBehaviour {
+
+    public Image starsLine;
+    public List<Sprite> starsSprites;
+
+    public Text pointCounter;
+
+    public Slider fuelSlider;
+    public Slider giftSlider;
+
+    public PlayerManagerScript playerManagerScript;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //if(playerManagerScript.currentGiftCount != giftSlider.value)
+        //{
+        //    giftSlider.value = playerManagerScript.currentGiftCount;
+        //}
+
+        //if (playerManagerScript.currentFuel != fuelSlider.value)
+        //{
+        //    fuelSlider.value = playerManagerScript.currentFuel;
+        //}
+
+        UpdateGiftSlider(playerManagerScript.currentGiftCount);
+        UpdateFuelSlider(playerManagerScript.currentFuel);
+        UpdateScore(playerManagerScript.score);
+        UpdateWantedIndicator(playerManagerScript.wantedIndicator);
+
+    }
+
+    public void UpdateWantedIndicator(int value)
+    {
+        starsLine.sprite = starsSprites[value];
+    }
+
+    public void UpdateScore(int value)
+    {
+        pointCounter.text = value.ToString();
+    }
+
+    public void UpdateFuelSlider(float value)
+    {
+        fuelSlider.value = value;
+    }
+
+    public void UpdateGiftSlider(float value)
+    {
+        giftSlider.value = value;
+    }
+
+    
+
+
+}
