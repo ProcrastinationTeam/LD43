1352840271697a8de28451c3c54f51c2ee36485f
Guillaume Ambrois <guillaume.ambrois@gmail.com>
2018-12-03 19:30:26 +0100

    Stairs

--- Assets/Scripts/StairsScript.cs
+++ Assets/Scripts/StairsScript.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class StairsScript : MonoBehaviour {
 
     public StairsScript target;
+    public List<StairsScript> origins = new List<StairsScript>();
+    public StairsTriggerScript stairsTriggerScript;
+
+    bool targetIsSafe = true;
 
     public Sprite outlined;
     Sprite previous;
@@ -13,11 +17,38 @@ public class StairsScript : MonoBehaviour {
     // Use this for initialization
     void Start () {
         sr = GetComponent<SpriteRenderer>();
+
+        GameObject[] gos = GameObject.FindGameObjectsWithTag("Elevator");
+        foreach(GameObject go in gos)
+        {
+            StairsScript tempStairsScript = go.GetComponent<StairsScript>();
+            if(tempStairsScript.target == this)
+            {
+                origins.Add(tempStairsScript);
+            }
+        }
     }
 	
 	// Update is called once per frame
 	void Update () {
-
+        if(targetIsSafe)
+        {
+            if (target.stairsTriggerScript.npcsInTrigger.Count > 0)
+            {
+                targetIsSafe = false;
+                Debug.Log(gameObject.name + " : NY VAS PAS");
+                // TODO: Lancer l'anim clignotante
+            }
+        } else
+        {
+            if (target.stairsTriggerScript.npcsInTrigger.Count == 0)
+            {
+                targetIsSafe = true;
+                Debug.Log(gameObject.name + " : gogo");
+                // TODO: Lancer l'idle
+            }
+        }
+        
     }
 
     void OnTriggerEnter2D(Collider2D col)
---
+++ Assets/StairsTriggerScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StairsTriggerScript : MonoBehaviour {
+
+    public List<PatrolScript> npcsInTrigger = new List<PatrolScript>();
+    public StairsScript parent;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Enemy"))
+        {
+            /*// S'il va vers l'entrée
+            float xVelocity = col.gameObject.GetComponent<Rigidbody2D>().velocity.x;
+            float xPosition = col.gameObject.transform.position.x;
+            if ((xVelocity > 0 && xPosition < parent.transform.position.x)
+                || (xVelocity < 0 && xPosition > parent.transform.position.x))
+            {*/
+                if(!npcsInTrigger.Contains(col.gameObject.GetComponent<PatrolScript>())) {
+                    npcsInTrigger.Add(col.gameObject.GetComponent<PatrolScript>());
+                }
+            //}
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        // S'il va vers l'entrée
+        float xVelocity = col.gameObject.GetComponent<Rigidbody2D>().velocity.x;
+        float xPosition = col.gameObject.transform.position.x;
+        if ((xVelocity > 0 && xPosition > parent.transform.position.x + 1)
+            || (xVelocity < 0 && xPosition < parent.transform.position.x - 1))
+            {
+            if (npcsInTrigger.Contains(col.gameObject.GetComponent<PatrolScript>()))
+            {
+                npcsInTrigger.Remove(col.gameObject.GetComponent<PatrolScript>());
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Enemy"))
+        {
+            npcsInTrigger.Remove(col.gameObject.GetComponent<PatrolScript>());
+        }
+    }
+}
